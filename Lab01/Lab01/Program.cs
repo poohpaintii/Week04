bbfa88e1040a5d6c148ba122d0c0fbd04a2d0a09
poohpaintii <poohpaintii@gmail.com>
2017-09-01 12:12:33 +0700

    E-W4P09

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace Lab01
 {
@@ -10,6 +7,10 @@ namespace Lab01
     {
         static void Main(string[] args)
         {
+            char ch;
+            Console.Write("Press a key followed by ENTER:");
+            ch = (char)Console.Read();  // get a char
+            Console.WriteLine("Your key is: " + ch);            
         }
     }
 }
