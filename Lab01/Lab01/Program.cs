b224688233e0d5775225dc5e826c0d947bc62229
poohpaintii <poohpaintii@gmail.com>
2017-09-01 10:14:15 +0700

    E-W4P02

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace Lab01
 {
@@ -10,6 +7,7 @@ namespace Lab01
     {
         static void Main(string[] args)
         {
+            Console.Write("Hello");
         }
     }
 }
