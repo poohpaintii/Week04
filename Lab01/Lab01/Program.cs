f2d32587ad50ef7333a0e03bc7d3340ecda769e1
poohpaintii <poohpaintii@gmail.com>
2017-09-01 11:33:59 +0700

    E-E4P06

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace Lab01
 {
@@ -10,6 +7,13 @@ namespace Lab01
     {
         static void Main(string[] args)
         {
+            int n = 123456789;            
+            Console.WriteLine("{0, 0:E}", n);
+            Console.WriteLine("{0, 0:F}", n);
+            Console.WriteLine("{0, 0:G}", n);
+            Console.WriteLine("{0, 0:N}", n);
+            Console.WriteLine("{0, 0:P}", n);
+            Console.WriteLine("{0, 0:X}", n);            
         }
     }
 }
