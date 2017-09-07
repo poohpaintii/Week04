f08370386e8be1b189beb497473bea72de888a43
poohpaintii <poohpaintii@gmail.com>
2017-09-07 15:19:04 +0700

    E-W4P19

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -6,10 +6,22 @@ using System.Threading.Tasks;
 
 namespace Lab01
 {
-    class Program
+    public class MathTest
     {
         static void Main(string[] args)
         {
+            for (float i = 0; i < Math.PI * 2.0F; i += 0.3F)
+            {
+                Console.WriteLine("The sine of {0,10:F} = {1,-10:F6}" +
+                spaces(Math.Sin(i)) + "*", i, Math.Sin(i));
+                Console .ReadKey ();
+            }
+        }
+        private static string spaces(double val)
+        {
+            string SpaceString = new String(' ', ((int)(val * 10.0)) + 10);
+            return SpaceString;
         }
     }
+
 }
