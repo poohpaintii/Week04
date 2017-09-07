f540da99802497760efc88faf2159c668e489e84
poohpaintii <poohpaintii@gmail.com>
2017-09-07 15:02:23 +0700

    E-W4P17

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace Lab01
 {
@@ -10,6 +7,17 @@ namespace Lab01
     {
         static void Main(string[] args)
         {
+            int a, b, c, d, e, f;
+            a = 1;
+            b = a + 6;
+            c = b - 3;
+            d = c * 2;
+            e = d / 2;
+            f = e % 2;
+            
+            Console.WriteLine("{0}", f);
+            Console.ReadKey();
+
         }
     }
 }
