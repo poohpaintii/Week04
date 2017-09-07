329cca70113a0afeb5b7a009a7fe69f6a0244af4
poohpaintii <poohpaintii@gmail.com>
2017-09-07 17:18:31 +0700

    edit

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -1,15 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
-namespace Lab01
+using System;
+public class MathTest
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-        }
+      
     }
 }
+
+
+
+
+
+
+
+
+
